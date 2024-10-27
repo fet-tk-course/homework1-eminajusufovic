b9c90a1c82a769710af008a8ee741599400533dd
Emina Jusufovic <emina.jusufovic@fet.ba>
2024-10-27 11:09:05 +0100

    Zadatak dodat u Homework1

--- Homework1/zad1-A.kt
+++ Homework1/zad1-A.kt
@@ -5,7 +5,7 @@ interface Person {
 
 }
 
-class Developer (val name : String, val srname : String, val age : Int, val ctry : String, val progLang : String) : Person{
+open class Developer (val name : String, val srname : String, val age : Int, val ctry : String, val progLang : List<String>) : Person{
 
     fun ageOfThePerson () : Int {
         return age
@@ -14,8 +14,36 @@ class Developer (val name : String, val srname : String, val age : Int, val ctry
     fun country () : String {
         return ctry
     }
+class BackendDeveloper (name : String, srname : String, age : Int, ctry : Int, progLang : List<String>, val backendFramework : String) : Developer (name, srname, age, ctry, progLang){
+
+}
+
+
+class FrontendDeveloper ( name : String, srname : String, age : Int, ctry : Int, progLang : List<String>, val frontendFramework : String) : Developer (name, srname, agre, ctry, progLang) {
+
+}
+
+
+    fun main() {
+        val developers = listOf (
+            BackendDeveloper ("Amila", listOf("Kotlin"), "Spring Boot")
+            BackendDeveloper ("Ibrahim", listOf("Java"), "Spring")
+            FrontendDeveloper ("Emina", listOf("Kotlin"), "React")
+            FrontendDeveloper ("Mujo", listOf("JavaScript"), "Vue.js")
+            FrontendDeveloper ("Edin", listOf("Kotlin"), "Ktor")
 
 
 
 
+
+        )
+
+
+
+
+
+
+
+
+    }
 }
